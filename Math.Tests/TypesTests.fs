90d4060d4dd653693aef90840868302757b745d8
Chris Gardner <cgardner@gmail.com>
2017-05-10 17:10:22 -0500

    Holy Holy, addition is working.
    Probably need some more tests, but I think I got all the parts.

---
+++ Math.Tests/TypesTests.fs
@@ -0,0 +1,45 @@
+﻿namespace Freestylecoding.Math.Tests
+
+open Xunit
+open Freestylecoding.Math.Types
+
+module Types =
+    [<Theory>]
+    [<InlineData( 1u, 1u, 2u )>]
+    [<InlineData( 1u, 5u, 6u )>]
+    [<InlineData( 9u, 2u, 11u )>]
+    let NaturalAddition l r s =
+        let l = Natural ([l])
+        let r = Natural ([r])
+        Assert.Equal( Natural ([s]), l + r )
+    
+    [<Fact>]
+    let NaturalAdditionOverflow () =
+        let l = Natural ([System.UInt32.MaxValue])
+        let r = Natural ([2u])
+        Assert.Equal( Natural ([1u; 1u]), l + r )
+        
+    [<Fact>]
+    let NaturalAdditionLeftBiggerNoOverflow () =
+        let l = Natural ([0xFu; 0xFu])
+        let r = Natural ([0xFF00u])
+        Assert.Equal( Natural ([0xFu; 0xFF0Fu]), l + r )
+        
+    [<Fact>]
+    let NaturalAdditionRightBiggerNoOverflow () =
+        let l = Natural ([0xFF00u])
+        let r = Natural ([0xFu; 0xFu])
+        Assert.Equal( Natural ([0xFu; 0xFF0Fu]), l + r )
+        
+    [<Fact>]
+    let NaturalAdditionCascadingOverflow () =
+        let l = Natural ([1u])
+        let r = Natural ([System.UInt32.MaxValue; System.UInt32.MaxValue])
+        Assert.Equal( Natural ([1u; 0u; 0u]), l + r )
+
+    [<Fact>]
+    let NaturalAdditionOverflowCausedOverflow () =
+        let l = Natural ([1u; 1u])
+        let r = Natural ([1u; System.UInt32.MaxValue - 1u; System.UInt32.MaxValue])
+        Assert.Equal( Natural ([2u; 0u; 0u]), l + r )
+        
--- Math/Types.fs
+++ Math/Types.fs
@@ -1,103 +1,41 @@
 ﻿namespace Freestylecoding.Math
 
 open System
-open System.Collections
 
 module Types = 
-    type Natural = uint32 list
+    type public Natural = Natural of uint32 list
 
-    let rec inline (+) (l:Natural) (r:Natural) : Natural =
-        let rec add (l:Natural) (r:Natural) : (Natural * bool) =
-            ([0u],false)
+    let (+) (left:Natural) (right:Natural) : Natural =
+        let getList n =
+            let (Natural l) = n
+            l
 
-        let (s,o) =
-            if l.Length < r.Length
-            then add r l
-            else add l r
+        let len =
+            Math.Max( (getList left).Length, (getList right).Length )
 
-        if o
-        then (s.Head + 1u) :: s.Tail
-        else s
+        let l = 
+            List.init (len - (getList left).Length) (fun i -> 0u)
+            |> List.append
+            <| (getList left)
+            |> List.rev
 
-    let (^^) l r =
-        (l || r) && not (l && r)
+        let r = 
+            List.init (len - (getList right).Length) (fun i -> 0u)
+            |> List.append
+            <| (getList right)
+            |> List.rev
 
-    //let rec public (+) (l:Natural) (r:Natural) : Natural =
-    //    match l.Length - r.Length with
-    //    | n when n < 0 -> r + l
-    //    | p when p > 0 -> l + ( false :: r )
-    //    | _ ->
-    //        match l.IsEmpty with
-    //        | true -> []
-    //        | false ->
-    //            let v = l.Tail + r.Tail
-    //            let o =
-    //                match l.Length = v.Length with
-    //                | true -> true
-    //                | false -> false
-    //            let s = (not l.Head) ^^ (r.Head ^^ o)
+        let f i x y =
+            match i with
+            | 0 -> x + y
+            | i ->
+                match l.[i-1] >= ( System.UInt32.MaxValue - r.[i-1] ) with
+                | true -> x + y + 1u
+                | false -> x + y
 
-    //            match (r.Head && o) || (l.Head && (r.Head || o )) with
-    //            | true -> true :: s :: v
-    //            | false -> s :: v
+        let s = 
+            List.mapi2 f l r
+            |> List.rev
 
-    //let rec public (-) (l:Natural) (r:Natural) : Natural =
-    //    []
-
-    //let rec public (*) (l:Natural) (r:Natural) : Natural =
-        //match r with
-        ////| 0 -> []
-        //| _ -> []
-
-type Integer( x : int ) =
-    let HighestBit = int ( Math.Floor( Math.Log( float x, 2. ) ) )
-
-    // Astute reader will notice the BitArray is packed in a Little-Endian format
-    // This is for two reasons
-    // 1) The index of the bit is the power of 2 for the value
-    // 2) When we do all the bitwise math, the indexes will match
-    let data = new BitArray( HighestBit )
-
-    do for i = 0 to HighestBit do
-        data.[i] <- (x &&& (1 <<< i)) > 0
-
-    // NOTE: This is only settable for the private constructor that uses ba
-    member val private Data = data with get, set
-
-    new() = Integer( 0 )
-    private new( ba:BitArray ) as this = Integer( 0 ) then this.Data <- ba
-
-    static member (+) ( l : Integer, r : Integer ) =
-        let s = new BitArray( Math.Max( l.Data.Length, r.Data.Length ) )
-        let mutable o = false;
-
-        let (^^) l r =
-            (l || r) && not (l && r)
-
-        let getBit (a:Integer) i =
-            if i >= a.Data.Length then false
-            else a.Data.[i]
-
-        for i in { 0 .. s.Length - 1 } do
-            s.[i] <- (not (getBit l i)) ^^ ((getBit r i) ^^ o)
-            o <- ((getBit r i) && o) || ((getBit l i) && ((getBit r i) || o ))
-
-        if o then
-            s.Length <- s.Length + 1
-            s.[s.Length - 1] <- true
-
-        Integer( s )
-
-    override this.Equals( obj ) = this.ToString() = obj.ToString()
-    override this.GetHashCode() = this.Data.GetHashCode()
-
-    override this.ToString() =
-        let rec pack i =
-            if this.Data.Length = i then 0
-            else
-                let v = (pack (i+1)) <<< 1
-                if this.Data.[i] then v ||| 0x1
-                else v
-
-        if 33 < this.Data.Length then (pack 0).ToString()
-        else raise (new NotImplementedException())
+        if s.Head = (List.min [ List.last l; List.last r; s.Head ]) then Natural ( 1u :: s )
+        else Natural ( s )
