62317e07fb092689128968b0f6d38db88b686a4a
Chris Gardner <cgardner@gmail.com>
2021-12-31 13:08:38 -0600

    Fixed a literal type specifier

--- Freestylecoding.Math.FSharp.Tests/NaturalTests.fs
+++ Freestylecoding.Math.FSharp.Tests/NaturalTests.fs
@@ -74,14 +74,14 @@ module Natural =
             Assert.Equal( Natural( [actual] ), Natural( actual ) )
 
         [<Theory>]
-        [<InlineData( 0x0000_0000UL, 0x0000_0000UL, 0x0000_0000_0000_0000UL )>]
-        [<InlineData( 0x0000_0000UL, 0x0000_0001UL, 0x0000_0000_0000_0001UL )>]
-        [<InlineData( 0x0000_0001UL, 0x0000_0000UL, 0x0000_0001_0000_0000UL )>]
-        [<InlineData( 0x0000_0001UL, 0x0000_0001UL, 0x0000_0001_0000_0001UL )>]
-        [<InlineData( 0x1200_0340UL, 0x0560_0078UL, 0x1200_0340_0560_0078UL )>]
-        [<InlineData( 0x1234_5678UL, 0x9ABC_DEF0UL, 0x1234_5678_9ABC_DEF0UL )>]
-        [<InlineData( 0xFFFF_0000UL, 0x0000_0000UL, 0xFFFF_0000_0000_0000UL )>]
-        [<InlineData( 0xFFFF_EEEEUL, 0xDDDD_CCCCUL, 0xFFFF_EEEE_DDDD_CCCCUL )>]
+        [<InlineData( 0x0000_0000u, 0x0000_0000u, 0x0000_0000_0000_0000uL )>]
+        [<InlineData( 0x0000_0000u, 0x0000_0001u, 0x0000_0000_0000_0001uL )>]
+        [<InlineData( 0x0000_0001u, 0x0000_0000u, 0x0000_0001_0000_0000uL )>]
+        [<InlineData( 0x0000_0001u, 0x0000_0001u, 0x0000_0001_0000_0001uL )>]
+        [<InlineData( 0x1200_0340u, 0x0560_0078u, 0x1200_0340_0560_0078uL )>]
+        [<InlineData( 0x1234_5678u, 0x9ABC_DEF0u, 0x1234_5678_9ABC_DEF0uL )>]
+        [<InlineData( 0xFFFF_0000u, 0x0000_0000u, 0xFFFF_0000_0000_0000uL )>]
+        [<InlineData( 0xFFFF_EEEEu, 0xDDDD_CCCCu, 0xFFFF_EEEE_DDDD_CCCCuL )>]
         let Uint64 (expectedHigh:uint32) (expectedLow:uint32) (actual:uint64) =
             Assert.Equal( Natural( [expectedHigh; expectedLow] ), Natural( actual ) )
 
