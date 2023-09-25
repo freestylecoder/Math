a671761b3db13383a2f8e9564ca4428321092783
Chris Gardner <cgardner@gmail.com>
2023-09-25 16:06:42 -0500

    rearranged a few files to get the tests to nest differently

---
+++ Freestylecoding.Math.FSharp.Tests/Natural/Constructors.fs
@@ -0,0 +1,47 @@
+﻿namespace Natural
+
+open Xunit
+open Freestylecoding.Math
+
+[<Trait( "Type", "Sanity" )>]
+type public Constructors() =
+    [<Theory>]
+    [<InlineData(                    "0", 0u, 0u, 0u )>]
+    [<InlineData(           "4294967297", 0u, 1u, 1u )>]
+    [<InlineData( "18446744073709551616", 1u, 0u, 0u )>]
+    [<InlineData( "18446744073709551617", 1u, 0u, 1u )>]
+    member public this.Default expected a b c =
+        Assert.Equal( Natural.Parse( expected ), Natural( [a; b; c] ) )
+
+    [<Theory>]
+    [<InlineData(   0u )>]
+    [<InlineData(   1u )>]
+    [<InlineData(   2u )>]
+    [<InlineData(   5u )>]
+    [<InlineData( 100u )>]
+    member public this.Uint32 (actual:uint32) =
+        Assert.Equal( Natural( [actual] ), Natural( actual ) )
+
+    [<Theory>]
+    [<InlineData( 0x0000_0000u, 0x0000_0000u, 0x0000_0000_0000_0000uL )>]
+    [<InlineData( 0x0000_0000u, 0x0000_0001u, 0x0000_0000_0000_0001uL )>]
+    [<InlineData( 0x0000_0001u, 0x0000_0000u, 0x0000_0001_0000_0000uL )>]
+    [<InlineData( 0x0000_0001u, 0x0000_0001u, 0x0000_0001_0000_0001uL )>]
+    [<InlineData( 0x1200_0340u, 0x0560_0078u, 0x1200_0340_0560_0078uL )>]
+    [<InlineData( 0x1234_5678u, 0x9ABC_DEF0u, 0x1234_5678_9ABC_DEF0uL )>]
+    [<InlineData( 0xFFFF_0000u, 0x0000_0000u, 0xFFFF_0000_0000_0000uL )>]
+    [<InlineData( 0xFFFF_EEEEu, 0xDDDD_CCCCu, 0xFFFF_EEEE_DDDD_CCCCuL )>]
+    member public this.Uint64 (expectedHigh:uint32) (expectedLow:uint32) (actual:uint64) =
+        Assert.Equal( Natural( [expectedHigh; expectedLow] ), Natural( actual ) )
+
+    [<Theory>]
+    [<InlineData( 0u, 0u, 0u )>]
+    [<InlineData( 0u, 0u, 1u )>]
+    [<InlineData( 0u, 1u, 0u )>]
+    [<InlineData( 0u, 1u, 1u )>]
+    [<InlineData( 1u, 0u, 0u )>]
+    [<InlineData( 1u, 0u, 1u )>]
+    [<InlineData( 1u, 1u, 0u )>]
+    [<InlineData( 1u, 1u, 1u )>]
+    member public this.Uint32Sequence (a:uint32) (b:uint32) (c:uint32) =
+        Assert.Equal( Natural( [ a; b; c ] ), Natural( seq { a; b; c } ) )
--- Freestylecoding.Math.FSharp.Tests/Natural/Sanity.fs
+++ Freestylecoding.Math.FSharp.Tests/Natural/Sanity.fs
@@ -1,50 +1,96 @@
-﻿namespace Freestylecoding.Math.FSharp.Natural.Tests
+﻿namespace Natural
 
 open Xunit
 open Freestylecoding.Math
 
+/// <summary>Sanity tests for Natural type</summary>
+/// <remarks>
+///     <para>
+///         The sanity tests are here to do low level tests of a few parts.
+///         This lets me isolate those tests so I can use those parts in the rest of the tests.
+///     </para>
+///     <para>
+///         In other words:
+///         IF ANY OF THE SANITY TESTS FAIL, DON'T TRUST ANY OTHER TEST!
+///     </para>
+/// </remarks>
 [<Trait( "Type", "Sanity" )>]
 type public Sanity() =
     [<Fact>]
     member public this.LeftShift () =
-        Assert.Equal( Natural([4u; 0x8000_0000u; 0u]), Natural([9u]) <<< 63 )
+        Assert.Equal(
+            Natural([4u; 0x8000_0000u; 0u]),
+            Natural([9u]) <<< 63
+        )
 
     [<Fact>]
     member public this.RightShift () =
-        Assert.Equal( Natural([9u]), Natural([4u; 0x8000_0000u; 0u]) >>> 63 )
+        Assert.Equal(
+            Natural([9u]),
+            Natural([4u; 0x8000_0000u; 0u]) >>> 63
+        )
 
     [<Fact>]
     member public this.GreaterThanTrue () =
-        Assert.True( Natural([0xDEADBEEFu; 0xBADu]) > Natural([0xBADu; 0xDEADBEEFu;]) )
+        Assert.True(
+            Natural([0xDEADBEEFu; 0xBADu]) > Natural([0xBADu; 0xDEADBEEFu;])
+        )
 
     [<Fact>]
     member public this.GreaterThanFalseByLessThan () =
-        Assert.False( Natural([0xBADu; 0xDEADBEEFu]) > Natural([0xDEADBEEFu; 0xBADu]) )
+        Assert.False(
+            Natural([0xBADu; 0xDEADBEEFu]) > Natural([0xDEADBEEFu; 0xBADu])
+        )
 
     [<Fact>]
     member public this.GreaterThanFalseByEquals () =
-        Assert.False( Natural([0xBADu; 0xDEADBEEFu]) > Natural([0xBADu; 0xDEADBEEFu]) )
+        Assert.False(
+            Natural([0xBADu; 0xDEADBEEFu]) > Natural([0xBADu; 0xDEADBEEFu])
+        )
 
     [<Fact>]
     member public this.Addition () =
-        Assert.Equal( Natural([2u; 0u; 0u]), Natural([1u; 1u]) + Natural([1u; System.UInt32.MaxValue - 1u; System.UInt32.MaxValue]) )
+        let maxUInt = System.UInt32.MaxValue
+        Assert.Equal(
+            Natural([2u; 0u; 0u]),
+            Natural([1u; 1u]) + Natural([1u; maxUInt - 1u; maxUInt])
+        )
 
     [<Fact>]
     member public this.Subtraction () =
-        Assert.Equal( Natural ([0xFFFF_FFFFu; 0xFFFF_FFFFu]), Natural([1u; 0u; 0u]) - Natural([1u]) )
+        let maxUInt = System.UInt32.MaxValue
+        Assert.Equal(
+            Natural ([maxUInt; maxUInt]),
+            Natural([1u; 0u; 0u]) - Natural([1u])
+        )
 
     [<Fact>]
     member public this.Multiplication () =
-        Assert.Equal( Natural([0x75CD9046u; 0x541D5980u]), Natural([0xFEDCBA98u]) * Natural([0x76543210u]) )
+        Assert.Equal(
+            Natural([0x75CD9046u; 0x541D5980u]),
+            Natural([0xFEDCBA98u]) * Natural([0x76543210u])
+        )
 
     [<Fact>]
     member public this.DivisionModulo () =
-        Assert.Equal( (Natural([0xFEDCBA98u]),Natural([0x12345678u])), Natural([0x75CD9046u; 0x6651AFF8u]) /% Natural([0x76543210u]) )
+        Assert.Equal(
+            (
+                Natural([0xFEDCBA98u]),
+                Natural([0x12345678u])
+            ),
+            Natural([0x75CD9046u; 0x6651AFF8u]) /% Natural([0x76543210u])
+        )
 
     [<Fact>]
     member public this.ToString () =
-        Assert.Equal( "1234567890123456789", Natural([0x112210F4u; 0x7DE98115u]).ToString() )
+        Assert.Equal(
+            "1234567890123456789",
+            Natural([0x112210F4u; 0x7DE98115u]).ToString()
+        )
 
     [<Fact>]
     member public this.Parse () =
-        Assert.Equal( Natural([0x112210F4u; 0x7DE98115u] ), Natural.Parse("1234567890123456789") )
+        Assert.Equal(
+            Natural([0x112210F4u; 0x7DE98115u] ),
+            Natural.Parse("1234567890123456789")
+        )
